41dce1f2ebf8d2e0bf8df563d53b70afe176979c
David Stosik <david.stosik+git-noreply@gmail.com>
2019-01-05 18:34:51 +0000

    Refactor spec for readability

--- spec/examples/by_set_pos_spec.rb
+++ spec/examples/by_set_pos_spec.rb
@@ -6,12 +6,13 @@ module IceCube
     it 'should behave correctly' do
       schedule = IceCube::Schedule.from_ical "RRULE:FREQ=MONTHLY;COUNT=4;BYDAY=WE;BYSETPOS=4"
       schedule.start_time = Time.new(2015, 5, 28, 12, 0, 0)
-      expect(schedule.occurrences_between(Time.new(2015, 01, 01), Time.new(2017, 01, 01))).to eq([
-                                                                                                     Time.new(2015,6,24,12,0,0),
-                                                                                                     Time.new(2015,7,22,12,0,0),
-                                                                                                     Time.new(2015,8,26,12,0,0),
-                                                                                                     Time.new(2015,9,23,12,0,0)
-                                                                                                 ])
+      expectations = [
+        Time.new(2015, 6, 24, 12, 0, 0),
+        Time.new(2015, 7, 22, 12, 0, 0),
+        Time.new(2015, 8, 26, 12, 0, 0),
+        Time.new(2015, 9, 23, 12, 0, 0)
+      ]
+      expect(schedule.occurrences(Time.new(2017, 01, 01))).to eq(expectations)
     end
 
   end
@@ -19,11 +20,12 @@ module IceCube
   describe YearlyRule, 'BYSETPOS' do
     it 'should behave correctly' do
       schedule = IceCube::Schedule.from_ical "RRULE:FREQ=YEARLY;BYMONTH=7;BYDAY=SU,MO,TU,WE,TH,FR,SA;BYSETPOS=-1"
-      schedule.start_time = Time.new(1966,7,5)
-      expect(schedule.occurrences_between(Time.new(2015, 01, 01), Time.new(2017, 01, 01))).to eq([
-                                                                                                     Time.new(2015, 7, 31),
-                                                                                                     Time.new(2016, 7, 31)
-                                                                                                 ])
+      schedule.start_time = Time.new(1966, 7, 5)
+      expectations = [
+        Time.new(2015, 7, 31),
+        Time.new(2016, 7, 31)
+      ]
+      expect(schedule.occurrences_between(Time.new(2015, 01, 01), Time.new(2017, 01, 01))).to eq(expectations)
     end
   end
 end
